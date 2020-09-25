ff483ad4e4de87cbcbb038fbe93d1bb51c254593
Atick Faisal <atickfaisal@gmail.com>
2020-09-26 01:05:00 +0600

    going into mqtt

--- app/src/main/java/ai/andromeda/griffin/home/HomeFragment.kt
+++ app/src/main/java/ai/andromeda/griffin/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ai.andromeda.griffin.R
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
@@ -15,11 +16,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_home, container, false)
-        rootView.fab.setOnClickListener {
-
-        }
+        rootView.fab.setOnClickListener { navigateToRegister() }
         return rootView
     }
 
-
+    private fun navigateToRegister() {
+        findNavController().navigate(R.id.action_homeFragment_to_registerFragment)
+    }
 }
